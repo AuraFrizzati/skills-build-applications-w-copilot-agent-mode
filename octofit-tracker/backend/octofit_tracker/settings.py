f61dea1a3b296bbadae3fd8e643b63ea0293ae57
Aura Frizzati <81711190+AuraFrizzati@users.noreply.github.com>
2025-10-21 18:46:13 +0000

    done

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-1s^1n%e4!+lz1kg5%eqzt^rz-h!6z6_f#aja-5kexjg^tuenv_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
